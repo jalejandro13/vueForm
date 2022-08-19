43c859395203c3d6984623d152a84523e4f660c2
Alejandro <jalejandrohdez13@gmail.com>
2022-08-19 16:09:27 -0500

    Se dio estilo al form y se agrego la logica para vaciar los inputs depues de enviar

--- index.js
+++ index.js
@@ -13,28 +13,40 @@ const app = Vue.createApp({
                     respuesta: ''
                 },
                 {
-                    etiqueta: 'Animal Favorito',
+                    etiqueta: 'Animal Fav',
                     tipo: 'text',
                     respuesta: ''
                 }
             ]
         }
     },
+    methods: {
+        agregar(){
+            this.informacionForm.forEach(input => console.log(input['respuesta']));
+            this.informacionForm.forEach(input => input['respuesta'] = '');
+            this.informacionForm.forEach(input => console.log(input['respuesta']));
+        }
+    },
 });
 
 app.component('input-div', {
     template: `
-        <div class="row justify-content-center">
-                <label class="col-auto" :for="etiqueta">{{ etiqueta }}:</label>
-                <input class="col-6" :type="tipo" :id="etiqueta" :name="etiqueta" >
+        <div class="row justify-content-center mt-3">
+                <label class="col-2" :for="etiqueta">{{ etiqueta }}:</label>
+                <input class="col-5" :type="tipo" :id="etiqueta" :name="etiqueta" v-model="respuesta">
         </div>
     `,
-    data() {
-        return {
-            respuesta: ''
+    props: ['etiqueta', 'tipo', 'modelValue'],
+    computed: {
+        respuesta: {
+            get(){
+                return this.modelValue;
+            },
+            set(res){
+                this.$emit('update:modelValue', res);
+            }
         }
-    },
-    props: ['etiqueta', 'tipo', 'modelValue']
+    }
 });
 
 app.mount('#app');
